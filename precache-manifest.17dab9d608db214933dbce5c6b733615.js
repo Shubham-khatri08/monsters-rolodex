c87a8ee54982a759d8925559b500ed47432cc000
Shubham Khatri <shubham.khatri08@gmail.com>
2020-04-23 11:20:46 +0530

    Updates

---
+++ precache-manifest.17dab9d608db214933dbce5c6b733615.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31131853bc9595da101584577c6ea9df",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "77f5dec185bab3af35fd",
+    "url": "/monsters-rolodex/static/css/main.0a6c3132.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77f5dec185bab3af35fd",
+    "url": "/monsters-rolodex/static/js/main.057fafe2.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
